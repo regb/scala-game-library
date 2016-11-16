f8e53e466357c5450655fea9dc9bbb32edd1c4c2
Régis Blanc <regwblanc@gmail.com>
2016-11-16 23:41:54 +0100

    new geometry features

--- core/src/main/scala/sgl/geometry/Point.scala
+++ core/src/main/scala/sgl/geometry/Point.scala
@@ -26,35 +26,3 @@ object Point {
   implicit def tupleToPoint(p: (Double, Double)): Point = Point(p._1, p._2)
   implicit def intTupleToPoint(p: (Int, Int)): Point = Point(p._1, p._2)
 }
-
-/*
- * We use different class for Point and Vec, even though
- * they can be seen as the same object, because it seems
- * like we don't want to accidently replace one by another
- */
-case class Vec(x: Double, y: Double) {
-
-  def +(m: Vec): Vec = Vec(x+m.x, y+m.y)
-  def -(m: Vec): Vec = Vec(x-m.x, y-m.y)
-
-  def *(s: Double): Vec = Vec(x*s, y*s)
-  
-  def unary_- : Vec = Vec(-x, -y)
-
-  def norm: Double = math.sqrt(x*x + y*y)
-
-  def normalized: Vec = {
-    val n = this.norm
-    Vec(x/n, y/n)
-  }
-
-  def isZero: Boolean = x == 0 && y == 0
-  def nonZero: Boolean = !isZero
-
-  def pmax(that: Vec): Vec = Vec(x max that.x, y max that.y)
-  def pmin(that: Vec): Vec = Vec(x min that.x, y min that.y)
-
-  def dotProduct(that: Vec): Double = this.x*that.x + this.y*that.y
-  def *(that: Vec): Double = this.dotProduct(that)
-
-}
---
+++ core/src/main/scala/sgl/geometry/Vec.scala
@@ -0,0 +1,43 @@
+package sgl.geometry
+
+//import scala.language.implicitConversions
+
+/*
+ * We use different class for Point and Vec, even though
+ * they can be seen as the same object, because it seems
+ * like we don't want to accidently replace one by another
+ */
+case class Vec(x: Double, y: Double) {
+
+  def +(m: Vec): Vec = Vec(x+m.x, y+m.y)
+  def -(m: Vec): Vec = Vec(x-m.x, y-m.y)
+
+  def *(s: Double): Vec = Vec(x*s, y*s)
+  
+  def unary_- : Vec = Vec(-x, -y)
+
+  def norm: Double = math.sqrt(x*x + y*y)
+
+  def normalized: Vec = {
+    val n = this.norm
+    Vec(x/n, y/n)
+  }
+
+  def isZero: Boolean = x == 0 && y == 0
+  def nonZero: Boolean = !isZero
+
+  def pmax(that: Vec): Vec = Vec(x max that.x, y max that.y)
+  def pmin(that: Vec): Vec = Vec(x min that.x, y min that.y)
+
+  def dotProduct(that: Vec): Double = this.x*that.x + this.y*that.y
+  def *(that: Vec): Double = this.dotProduct(that)
+
+  //TODO: could define +=, *=, -= as mutating the Vec? would make much
+  //      code similar, we would still keep +/*/- as operation that
+  //      produces new Vec, but there would be the option of not creating garbage
+
+  //clockwise as the standard mathematical interpetation (with y pointing up),
+  //so might be reversed in the game engine world
+  def clockwisePerpendicular: Vec = Vec(-y, x)
+
+}
