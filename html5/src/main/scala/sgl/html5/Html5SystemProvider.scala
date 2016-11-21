6927675f325496216d3e4f99400f719a2dad419d
Régis Blanc <regwblanc@gmail.com>
2016-11-21 01:11:30 +0100

    provide loading for text resources

--- html5/src/main/scala/sgl/html5/Html5SystemProvider.scala
+++ html5/src/main/scala/sgl/html5/Html5SystemProvider.scala
@@ -9,7 +9,27 @@ trait Html5SystemProvider extends SystemProvider {
 
   override def exit(): Unit = {}
 
-  override def loadTextResource(path: String): Iterator[String] = ???
+
+  //probably cleaner to return lazily and block only when iterator is called
+  //class LazyTextResource(rawFile: dom.XMLHttpRequest) extends Iterator[String] = {
+
+  //}
+  //but the best would be to redefine these loading APIs to be async
+
+  override def loadTextResource(path: String): Iterator[String] = {
+    var res: Iterator[String] = null
+    val rawFile = new dom.XMLHttpRequest()
+    rawFile.open("GET", "static/" + path, false)
+    rawFile.onreadystatechange = (event: dom.Event) => {
+      if(rawFile.readyState == 4) {
+        if(rawFile.status == 200 || rawFile.status == 0) {
+          res = rawFile.responseText.split("\n").iterator
+        }
+      }
+    }
+    rawFile.send(null)
+    res
+  }
 
   override def openWebpage(uri: URI): Unit = {
     dom.window.open(uri.toString)
