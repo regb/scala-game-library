dacb1daf855135bb77a8d36026101630538b1eeb
Régis Blanc <regwblanc@gmail.com>
2016-11-26 15:47:52 +0100

    cleaner setinterval game loop

--- examples/test/html5/src/main/scala/Main.scala
+++ examples/test/html5/src/main/scala/Main.scala
@@ -3,6 +3,7 @@ package html5
 
 import sgl.html5._
 import sgl.html5.util._
+import sgl.html5.themes._
 import sgl._
 
 import scala.scalajs.js.annotation.JSExport
@@ -11,6 +12,8 @@ import scala.scalajs.js.annotation.JSExport
 object Main extends core.AbstractApp with Html5App 
   with InputHelpersComponent with Html5VerboseConsoleLoggingProvider {
 
-  override val CanvasDimension = Some((800, 400))
+  override val theme = new DefaultTheme {
+    override val maxFrame = (800, 400)
+  }
 
 }
--- html5/src/main/scala/sgl/html5/Html5GameLoopProvider.scala
+++ html5/src/main/scala/sgl/html5/Html5GameLoopProvider.scala
@@ -1,14 +1,16 @@
 package sgl
 package html5
 
-import util._
+import sgl.util._
 
 import scala.scalajs.js
 import org.scalajs.dom
 import dom.html
 
 trait Html5GameLoopProvider extends GameLoopProvider {
-  self: Html5GraphicsProvider with GameStateComponent =>
+  self: Html5GraphicsProvider with GameStateComponent with LoggingProvider =>
+
+  private implicit val Tag = Logger.Tag("game-loop")
 
   //no code automatically run here, it doesn't seem very safe to rely on Lifecycle to add code on
   //the framework (given that the exact call ordering is dubious) so I'd rather
@@ -16,16 +18,15 @@ trait Html5GameLoopProvider extends GameLoopProvider {
   //else. 
   //TODO: Probably means that we don't need the concept of a GameLoopProvider in the framework then
 
+
   def startGameLoop(): Unit = {
     //TODO: use requestAnimationFrame
-    //TODO: actually use the FPS
     
     //only at startup, we add the starting screen to the game state
     gameState.newScreen(startingScreen)
 
     var lastTime: Long = js.Date.now.toLong
-    dom.window.setInterval(() => {
-
+    def frameCode(): Unit = {
       val now = js.Date.now.toLong
       val dt: Long = now - lastTime
       lastTime = now
@@ -33,9 +34,20 @@ trait Html5GameLoopProvider extends GameLoopProvider {
       val canvas = getScreenCanvas
       gameLoopStep(dt, canvas)
       releaseScreenCanvas(getScreenCanvas)
+      
+      FramePeriod.foreach(framePeriod => {
+        //we check the time from the begininning of the frame until the end. The exact
+        //dt sent to the update function will be different, as it depends on when the
+        //setInterval fires the code, and might slightly overflow the target FPS
+        val frameTime = js.Date.now.toLong - now
+        if(frameTime > framePeriod)
+          logger.warning("Frame took too long to execute. Losing FPS. Frame time: " + frameTime)
+      })
+    }
 
-    }, 40)
+    dom.window.setInterval(() => frameCode(), FramePeriod.map(_.toDouble).getOrElse(10d))
   }
+
 
   //TODO: maybe we need to stop this game loop if we support the pause when leaving browser tab. Not
   //      sure we should detect that anyway, as this is usually not the most reliable thing in the browser
