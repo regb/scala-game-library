eef9f736d1b4edeeba6ddd4ae902a824416d3167
Regis Blanc <regwblanc@gmail.com>
2020-05-17 22:18:27 +0200

    add test for previous commit which added support for url param in links to android app

---
+++ core/src/test/scala/sgl/SystemProviderSuite.scala
@@ -0,0 +1,32 @@
+package sgl
+
+import org.scalatest.FunSuite
+
+class SystemProviderSuite extends FunSuite {
+
+  object InstrumentedSystemProvider extends TestSystemProvider {
+    var instrumentedUri: java.net.URI = null
+    class InstrumentedTestSystem extends TestSystem {
+      override def openWebpage(uri: java.net.URI): Unit = {
+        instrumentedUri = uri
+      }
+    }
+    override val System = new InstrumentedTestSystem
+  }
+
+  test("openGooglePlayApp defaults to the correct URL without parameters") {
+    InstrumentedSystemProvider.System.openGooglePlayApp("com.regblanc.sgl")
+    val want = new java.net.URI("https://play.google.com/store/apps/details?id=com.regblanc.sgl")
+    assert(InstrumentedSystemProvider.instrumentedUri == want)
+  }
+
+  test("openGooglePlayApp defaults to the correct URL with parameters") {
+    InstrumentedSystemProvider.System.openGooglePlayApp("com.regblanc.sgl", Map("a" -> "b"))
+    val want = new java.net.URI("https://play.google.com/store/apps/details?id=com.regblanc.sgl&a=b")
+    assert(InstrumentedSystemProvider.instrumentedUri == want)
+    InstrumentedSystemProvider.System.openGooglePlayApp("com.regblanc.sgl", Map("a" -> "b", "c" -> "d"))
+    val want2 = new java.net.URI("https://play.google.com/store/apps/details?id=com.regblanc.sgl&a=b&c=d")
+    assert(InstrumentedSystemProvider.instrumentedUri == want2)
+  }
+
+}
