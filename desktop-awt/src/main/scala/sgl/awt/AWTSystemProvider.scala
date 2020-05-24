regb/scala-game-library

commit 26c271f9e5866c5b17c0bd56455be26e171cedf2

support multiple audio format

desktop-awt/src/main/scala/sgl/awt/AWTSystemProvider.scala
@@ -63,6 +63,8 @@ trait AWTSystemProvider extends SystemProvider with PartsResourcePathProvider {
   }
   val System = AWT5System
 
+  override val ResourcesRoot = PartsResourcePath(Vector(""))
+
   /** Control whether resources can be provided dynamically.
     *
     * The standard way to provide resources/assets is by packaging them in the

android/core/src/main/scala/sgl/android/AndroidAudioProvider.scala
@@ -19,6 +19,13 @@ trait AndroidAudioProvider extends Activity with AudioProvider {
 
   private implicit val LogTag = Logger.Tag("sgl-audio-provider")
 
+  /** The list of supported audio format on Android.
+    *
+    * The load methods are going to pick the first resource that
+    * matches any of these, and include it.
+    */
+  val SupportedAudioFormats: Set[String] = Set("ogg", "m4a", "mp3")
+
   // Callbacks to the audio module can come from the Activity lifecycle
   // so we want to make sure there is no concurrency going on there.
   private object AudioLocker
@@ -154,17 +161,20 @@ trait AndroidAudioProvider extends Activity with AudioProvider {
       }
 
     }
-    override def loadSound(path: ResourcePath): Loader[Sound] = {
+    override def loadSound(path: ResourcePath, extras: ResourcePath*): Loader[Sound] = {
       // We are essentilally initing the sound pool only once for the system. But because the
       // cake initialization is sometimes a bit hard to predict, moving this here makes it
       // easier to track when the initialization code happens.
       initSoundPool()
 
+      // Get the first resource that matches, otherwise try with the default one anyway.
+      val chosenResource: ResourcePath = (path +: extras).find(p => SupportedAudioFormats.contains(p.extension.getOrElse(""))).getOrElse(path)
+
       val loader = new DefaultLoader[Sound]()
 
       try {
         val am = self.getAssets()
-        val afd = am.openFd(path.path)
+        val afd = am.openFd(chosenResource.path)
         val soundId = soundPool.load(afd, 1)
         afd.close()
 
@@ -172,13 +182,13 @@ trait AndroidAudioProvider extends Activity with AudioProvider {
           if(status == 0) {
             loader.success(new Sound(soundId, 0, 1f, null))
           } else {
-            loader.failure(new RuntimeException(s"Sound ${path} failed to load with status: ${status}"))
+            loader.failure(new RuntimeException(s"Sound ${chosenResource} failed to load with status: ${status}"))
           }
         })
         loader
       } catch {
         case (e: java.io.IOException) =>
-          loader.failure(new ResourceNotFoundException(path))
+          loader.failure(new ResourceNotFoundException(chosenResource))
       }
     }
 
@@ -581,16 +591,18 @@ trait AndroidAudioProvider extends Activity with AudioProvider {
 
     }
 
-    override def loadMusic(path: ResourcePath): Loader[Music] = AudioLocker.synchronized {
+    override def loadMusic(path: ResourcePath, extras: ResourcePath*): Loader[Music] = AudioLocker.synchronized {
       val loader = new DefaultLoader[Music]
+      // Get the first resource that matches, otherwise try with the default one anyway.
+      val chosenResource = (path +: extras).find(p => SupportedAudioFormats.contains(p.extension.getOrElse(""))).getOrElse(path)
       try {
-        val music = new Music(path)
+        val music = new Music(chosenResource)
         loader.success(music)
         addLoadedMusic(music)
         if(activityPaused) // If this was paused in a race, make sure we freeze that music too.
           music.freezeOnPause()
       } catch {
-        case (e: java.io.IOException) => loader.failure(new ResourceNotFoundException(path))
+        case (e: java.io.IOException) => loader.failure(new ResourceNotFoundException(chosenResource))
       }
       loader
     }

build.sbt
@@ -185,7 +185,7 @@ lazy val helloHtml5 = (project in file("./examples/hello/html5"))
     name := "hello-html5",
     scalaJSUseMainModuleInitializer := true
   )
-  .dependsOn(coreJS, html5, helloCoreJS)
+  .dependsOn(coreJS, html5, html5Firebase, helloCoreJS)
 
 lazy val helloDesktopNative = (project in file("./examples/hello/desktop-native"))
   .enablePlugins(ScalaNativePlugin)
@@ -363,7 +363,7 @@ def isMac(name: String): Boolean = name.startsWith(MacName.toLowerCase)
  */
 lazy val verifyCiCommand = List(
   "coreJVM","desktopAWT","desktopNative","helloCoreJS","helloCoreJVM","helloCoreNative",
-  "helloDesktopAWT","helloDesktopNative","helloHtml5","html5","jvmShared","menuCoreJS",
+  "helloDesktopAWT","helloDesktopNative","helloHtml5","html5","html5Firebase","jvmShared","menuCoreJS",
   "menuCoreJVM","menuCoreNative","menuDesktopAWT","platformerCoreJS","platformerCoreJVM",
   "platformerCoreNative","platformerDesktopAWT","snakeCoreJS","snakeCoreJVM","snakeCoreNative",
   "snakeDesktopAWT","snakeDesktopNative","snakeHtml5"

core/src/main/scala/sgl/AudioProvider.scala
@@ -198,8 +198,17 @@ trait AudioProvider {
       *
       * The default config for the loaded sound should be a normal rate and no loop
       * (play just once, loop=0 in the withConfig).
+      *
+      * You can provide extra resource path, which should be the same sound but with
+      * an alternative format (inferred from the filetype). Dependeing on the backend
+      * we will use the most reliable format.
+      * TODO: make the filetype as a typed argument.
       */
-    def loadSound(path: ResourcePath): Loader[Sound]
+    def loadSound(path: ResourcePath, extras: ResourcePath*): Loader[Sound]
+    def loadSound(pathes: Seq[ResourcePath]): Loader[Sound] = {
+      require(pathes.size >= 1)
+      loadSound(pathes.head, pathes.tail:_*)
+    }
   
     /*
      * Music has a similar interface to sound, but is meant to load
@@ -291,8 +300,17 @@ trait AudioProvider {
       * to play. The Loader can fail with ResourceNotFoundException or
       * ResourceFormatUnsupportedException. The format supports varies per
       * platform and configuration.
+      *
+      * You can provide extra resource path, which should be the same music but with
+      * an alternative format (inferred from the filetype). Dependeing on the backend
+      * we will use the most reliable format.
+      * TODO: make the filetype as a typed argument.
       */
-    def loadMusic(path: ResourcePath): Loader[Music]
+    def loadMusic(path: ResourcePath, extras: ResourcePath*): Loader[Music]
+    def loadMusic(pathes: Seq[ResourcePath]): Loader[Music] = {
+      require(pathes.size >= 1)
+      loadMusic(pathes.head, pathes.tail:_*)
+    }
 
   }
   val Audio: Audio
@@ -333,7 +351,7 @@ trait FakeAudioProvider extends AudioProvider {
       override def endLoop(id: PlayedSound): Unit = {}
     }
 
-    override def loadSound(path: ResourcePath): Loader[Sound] = Loader.successful(new Sound)
+    override def loadSound(path: ResourcePath, extras: ResourcePath*): Loader[Sound] = Loader.successful(new Sound)
 
     class Music extends AbstractMusic {
       override def play(): Unit = {}
@@ -344,7 +362,7 @@ trait FakeAudioProvider extends AudioProvider {
       override def dispose(): Unit = {}
     }
 
-    override def loadMusic(path: ResourcePath): Loader[Music] = Loader.successful(new Music)
+    override def loadMusic(path: ResourcePath, extras: ResourcePath*): Loader[Music] = Loader.successful(new Music)
   }
   override val Audio = FakeAudio
 }

core/src/main/scala/sgl/SystemProvider.scala
@@ -175,6 +175,11 @@ trait SystemProvider {
       * output as {{{ "a" / "c" }}}.
       */
     def / (filename: String): ResourcePath
+
+    def / (filenames: Seq[String]): Seq[ResourcePath] = filenames.map(f => this / f)
+
+    /** Returns the file extension. */
+    def extension: Option[String]
   }
 
   /** A path to access a resource in the system.
@@ -257,22 +262,26 @@ trait PartsResourcePathProvider {
       */
     def path: String = parts.mkString("/")
 
-    def / (filename: String): ResourcePath = {
+    override def / (filename: String): ResourcePath = {
       val subparts = filename.split("/")
-      this / subparts
+      join(this, subparts)
     }
 
-    def / (subparts: Seq[String]): ResourcePath = if(subparts.isEmpty) this else {
+    private def join(that: PartsResourcePath, subparts: Seq[String]): ResourcePath = if(subparts.isEmpty) that else {
       val r = subparts.head match {
-        case "." => this
+        case "." => that
         case ".." => PartsResourcePath(if(parts.isEmpty) parts else parts.init)
         case file => {
           PartsResourcePath(parts :+ file)
         }
       }
-      r / subparts.tail
+      join(r, subparts.tail)
+    }
+
+    override def extension: Option[String] = {
+      val i = parts.last.lastIndexOf('.')
+      if(i > 0) Some(parts.last.substring(i+1)) else None
     }
   }
   type ResourcePath = PartsResourcePath
-  override val ResourcesRoot = PartsResourcePath(Vector())
 }

core/src/test/scala/sgl/SystemProviderSuite.scala
@@ -29,4 +29,27 @@ class SystemProviderSuite extends AnyFunSuite {
     assert(InstrumentedSystemProvider.instrumentedUri == want2)
   }
 
+
+  object PartsResourcePathSystemProvider extends TestSystemNoResourcePathProvider with PartsResourcePathProvider {
+    override val ResourcesRoot = PartsResourcePath(Vector("root"))
+  }
+
+  test("PartsResourcePath creates the correct path") {
+    val r = PartsResourcePathSystemProvider.ResourcesRoot / "a" / "b" / "c.txt"
+    assert(r.path === "root/a/b/c.txt")
+  }
+
+  test("PartsResourcePath creates the correct pathes with multiple filenames") {
+    val rs = PartsResourcePathSystemProvider.ResourcesRoot / "a" / "b" / Seq("c.txt", "d.txt")
+    assert(rs.size === 2)
+    assert(rs(0).path === "root/a/b/c.txt")
+    assert(rs(1).path === "root/a/b/d.txt")
+  }
+
+  test("PartsResourcePath returns proper extension") {
+    val r = PartsResourcePathSystemProvider.ResourcesRoot / "a" / "b" / "c.txt"
+    assert(r.extension === Some("txt"))
+    val r2 = PartsResourcePathSystemProvider.ResourcesRoot / "a" / "b" / "c"
+    assert(r2.extension === None)
+  }
 }

core/src/test/scala/sgl/TestSystemProvider.scala
@@ -4,7 +4,19 @@ import sgl.util._
 
 import scala.util._
 
-trait TestSystemProvider extends SystemProvider {
+trait TestSystemProvider extends TestSystemNoResourcePathProvider {
+
+  class TestResourcePath extends AbstractResourcePath {
+    def / (filename: String): ResourcePath = ???
+
+    override def extension: Option[String] = ???
+  }
+  type ResourcePath = TestResourcePath
+  override val ResourcesRoot: ResourcePath = new TestResourcePath
+
+}
+
+trait TestSystemNoResourcePathProvider extends SystemProvider {
 
   class TestSystem extends System {
 
@@ -22,12 +34,5 @@ trait TestSystemProvider extends SystemProvider {
 
   }
   val System = new TestSystem
-
-  class TestResourcePath extends AbstractResourcePath {
-    def / (filename: String): ResourcePath = ???
-  }
-  type ResourcePath = TestResourcePath
-
-  override val ResourcesRoot: ResourcePath = new TestResourcePath
 
 }

desktop-awt/src/main/scala/sgl/awt/AWTAudioProvider.scala
@@ -254,7 +254,7 @@ trait AWTAudioProvider extends AudioProvider {
       
     }
   
-    override def loadSound(path: ResourcePath): Loader[Sound] = FutureLoader {
+    override def loadSound(path: ResourcePath, extras: ResourcePath*): Loader[Sound] = FutureLoader {
       logger.info("Loading sound resource: " + path.path)
       val resourceAudioStream = loadAudioInputStream(path)
       val (convertedAudioStream, _) = convertToBestStream(resourceAudioStream).getOrElse{
@@ -300,7 +300,8 @@ trait AWTAudioProvider extends AudioProvider {
           clip.close()
       }
     }
-    override def loadMusic(path: ResourcePath): Loader[Music] = FutureLoader {
+    // AWT backend always uses the first option.
+    override def loadMusic(path: ResourcePath, extras: ResourcePath*): Loader[Music] = FutureLoader {
       logger.info("Loading music resource: " + path.path)
       val resourceAudioStream = loadAudioInputStream(path)
       val (convertedAudioStream, clip) = convertToBestStream(resourceAudioStream).getOrElse{

commit 08dbb0a68a6621749598e98435c6beb29b99f0f4

Fix a bug introduced by refactoring of Resources
This was introduced by commit 26c271f9e58, when the
default ResourcesRoot definition was moved to the AWT
backend instead of in the base implementation, but it
wasn't correctly copied.

desktop-awt/src/main/scala/sgl/awt/AWTSystemProvider.scala
@@ -63,7 +63,7 @@ trait AWTSystemProvider extends SystemProvider with PartsResourcePathProvider {
   }
   val System = AWT5System
 
-  override val ResourcesRoot = PartsResourcePath(Vector(""))
+  override val ResourcesRoot = PartsResourcePath(Vector())
 
   /** Control whether resources can be provided dynamically.
     *