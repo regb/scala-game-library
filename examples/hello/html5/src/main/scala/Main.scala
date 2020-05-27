fad200112b717601cfdbea5843d2bac751162115
Regis Blanc <regwblanc@gmail.com>
2020-05-28 01:31:52 +0200

    fix the build

--- examples/hello/html5/src/main/scala/Main.scala
+++ examples/hello/html5/src/main/scala/Main.scala
@@ -6,10 +6,8 @@ import sgl.html5.util._
 import sgl.html5.themes._
 import sgl._
 
-import sgl.html5.analytics._
-
 object Main extends core.AbstractApp with Html5App 
-  with InputHelpersComponent with Html5VerboseConsoleLoggingProvider with Html5FirebaseAnalyticsProvider {
+  with InputHelpersComponent with Html5VerboseConsoleLoggingProvider {
 
   override val TargetFps = None
 
@@ -19,9 +17,4 @@ object Main extends core.AbstractApp with Html5App
     override val maxFrame = (800, 800)
   }
 
-  //Analytics.logCustomEvent("heythere", sgl.analytics.EventParams())
-  //Analytics.logLevelUpEvent(27)
-  //Analytics.setPlayerProperty("a", "b")
-  //Analytics.logPostScoreEvent(42, None, Some("abcd"))
-  //Analytics.logCustomEvent("my_custom_event", sgl.analytics.EventParams(itemId = Some("itema"), customs = Map("x" -> "y", "x2" -> "y2")))
 }
