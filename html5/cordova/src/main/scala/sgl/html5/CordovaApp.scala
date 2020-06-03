3be769cc060cb6eb05cb0ea453e231f7e403dfcc
Regis Blanc <regwblanc@gmail.com>
2020-06-04 01:04:49 +0200

    cordova app uses the audionative plugin by default

--- html5/cordova/src/main/scala/sgl/html5/CordovaApp.scala
+++ html5/cordova/src/main/scala/sgl/html5/CordovaApp.scala
@@ -2,14 +2,13 @@ package sgl.html5
 
 import scala.scalajs.js
 
-trait CordovaApp extends Html5App with CordovaMediaAudioProvider {
-//trait CordovaApp extends Html5App with CordovaNativeAudioAudioProvider {
+// trait CordovaApp extends Html5App with CordovaMediaAudioProvider {
+trait CordovaApp extends Html5App with CordovaNativeAudioAudioProvider {
 
-  //override val Audio: Audio = CordovaNativeAudioAudio
-  override val Audio: Audio = CordovaMediaAudio
+  override val Audio: Audio = CordovaNativeAudioAudio
+  // override val Audio: Audio = CordovaMediaAudio
 
   override def main(args: Array[String]): Unit = {
-    println("main cordova")
     js.Dynamic.global.document.addEventListener("deviceready", () => {
       super.main(args)
     })
