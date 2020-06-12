648b7fd6afc48d5d8db50441bc82d7acc47a9a59
Regis Blanc <regwblanc@gmail.com>
2020-06-13 00:50:15 +0200

    improve viewport (and fix and test)

--- core/src/main/scala/sgl/Viewport.scala
+++ core/src/main/scala/sgl/Viewport.scala
@@ -8,32 +8,36 @@ trait ViewportComponent {
   /** A screen view of the virtual world.
     *
     * Viewport is meant to map a virtual world into a physical screen. It
-    * essentially exports a mapping from two coordinates systems. Given that
+    * essentially exports a mapping from two coordinate systems. Given that
     * it's 2D, it's a relatively straightforward mapping, but the class is
     * convenient as an abstraction of how to render the virtual world into the
     * screen, and is used by various components of the framework as a standard
-    * way to express this mapping. For example, the scene extension needs the
-    * notion of a viewport to properly map click events to the right
-    * coordinates.
+    * way to express this mapping. For example, the [[sgl.scene]] extension
+    * needs the notion of a viewport to properly map click events (on the pixels
+    * on the screen) to the right world coordinates.
     *
     * The Viewport is constructed with the screen coordinates, screenWidth, and
     * screenHeight. In general, this should be the Window.width and
     * Window.height, but you can use the viewport for various mapping strategy
-    * so it could be something else (think split screens for example). The
-    * coordinates of the physical screen are discrete, since the screen is
-    * made of individual pixels. The dimensions are the number of pixels in
-    * both width and height.
+    * so it could be something else (think split screens for example).  The
+    * coordinates of the physical screen are discrete, since the screen is made
+    * of individual pixels. The dimensions are the number of pixels in both
+    * width and height.
     *
-    * Once created, we need to set a camera, which is essentially the world
-    * camera and thus define the dimensions of the world that we wish to
-    * render. The camera is thus not a camera within the viewport, it's the
-    * camera coordinates in a virtual world (which you should maintain in your
-    * game logic) and you set it to the viewport in order to be able to query
-    * the coordinate mapping. If you don't set a camera, it defaults to top-left
-    * and the entire screen (1:1 mapping to the screen). The camera is defined
-    * in floating point coordinates, as these are the world coordinates, and
-    * they could be arbitrary (for example, the whole world could be defined
-    * within [0,1] and the mapping to a physical viewport should do the rest).
+    * Once created, we need to set a camera, which represents the world and how 
+    * which part to map into the viewport. It defines the dimensions of the world that we
+    * render. It can be smaller/larger and potentially it might not have the same
+    * aspect ratio. It should match a convenient size for your game, and you use
+    * [[Viewport.withViewport]] to prepare your canvas before rendering your world.
+    * By setting up the camera, you can also explicitly query the coordinates mapping
+    * from world to screen and screen to world.
+    *
+    * If you don't set a camera, it defaults to top-left and the entire screen
+    * (1:1 mapping to the screen). The camera is defined in floating point
+    * coordinates, as these are the world coordinates, and they could be
+    * arbitrary (for example, the whole world could be defined within [0,1] and
+    * the mapping to a physical viewport should do the rest). The axis of the
+    * camera are the same as the underlying canvas axis.
     *
     * You can use the camera as the state of your scrolling implementation. If
     * you create a runner, and it moves horizontally all the time, you can simply
@@ -42,25 +46,28 @@ trait ViewportComponent {
     * for mapping your current world coordinates to the screen and render the
     * right part of world on the screen.
     *
-    * You can also handle the camera yourself, in your virtual coordinate system, and
-    * use the viewport just as a mapping from your virtual world dimensions to 
-    * the actual screen dimensions. An example of this is the scene.ui.ScrollPane
-    * which maintain its own internal camera system for scrolling, and is at a fixed
-    * coordinates. In that case, you would use the viewport by mapping the physical size
-    * of the ScrollPane (not the virtual scrolled world, but the actual paneWidth and
-    * paneHeight dimensions that the pane occupies) to the screen coordinates, and the
-    * viewport camera would never need to move (it would still need to be set to the
-    * dimensions of the ScrollPane, but origin would stay (0,0)).
+    * You can also handle the camera yourself, in your virtual coordinate
+    * system, and use the viewport just as a mapping from your virtual world
+    * dimensions to the actual screen dimensions. An example of this is the
+    * scene.ui.ScrollPane which maintain its own internal camera system for
+    * scrolling, and is at a fixed coordinates. In that case, you would use the
+    * viewport by mapping the physical size of the ScrollPane (not the virtual
+    * scrolled world, but the actual paneWidth and paneHeight dimensions that
+    * the pane occupies) to the screen coordinates, and the viewport camera
+    * would never need to move (it would still need to be set to the dimensions
+    * of the ScrollPane, but origin would stay (0,0)).
     * 
-    * Finally, you can set the scalingStrategy, which is a configuration of the
-    * mapping from world to screen. This lets you decide if you want to stretch
-    * the world to fill the screen, to fit it by keeping aspect ratio but at
-    * the cost of black borders, or to just do nothing and map it 1:1. If not
-    * set, it defaults to NoScaling (1:1).
+    * Finally, you can set the [[Viewport.scalingStrategy]], which is a
+    * configuration of the mapping from world to screen. This lets you decide
+    * if you want to stretch the world to fill the screen, to fit it by keeping
+    * aspect ratio but at the cost of black borders, or to just do nothing and
+    * map it 1:1. If not set, it defaults to NoScaling (1:1).
     */
   class Viewport(screenWidth: Int, screenHeight: Int) {
 
+    /** The width of the screen to which the viewport maps. */
     def width: Int = screenWidth
+    /** The height of the screen to which the viewport maps. */
     def height: Int = screenHeight
   
     // Top-left position of the camera clip.
@@ -68,6 +75,7 @@ trait ViewportComponent {
     private var _cameraY: Float = 0
     private var _cameraWidth: Float = screenWidth
     private var _cameraHeight: Float = screenHeight
+
     def cameraX: Float = _cameraX
     def cameraY: Float = _cameraY
     def cameraWidth: Float = _cameraWidth
@@ -82,20 +90,40 @@ trait ViewportComponent {
     def setCamera(x: Float, y: Float, w: Float, h: Float): Unit = {
       _cameraX = x
       _cameraY = y
+
       val newDim = w != _cameraWidth || h != _cameraHeight
       _cameraWidth = w
       _cameraHeight = h
-      if(newDim) // only needs to update when the w/h changes.
+      if(newDim) // Only needs to update when the width or height change.
         update()
     }
 
+    /** Translate the camera origin by dx and dy.
+      *
+      * This maintains the same camera total size, but translates
+      * the origin by the amount specified in dx and dy.
+      */
     def translateCamera(dx: Float, dy: Float): Unit = {
       _cameraX += dx
       _cameraY += dy
     }
 
+    /** Move the camera origin to (x, y).
+      *
+      * This maintains the same camera total size, but moves
+      * the origin to the coordinates x and y.
+      */
+    def translateCameraTo(x: Float, y: Float): Unit = {
+      _cameraX = x
+      _cameraY = y
+    }
+
     private var _scalingStrategy: ScalingStrategy = NoScaling
+
+    /** The scaling strategy used when mapping the virtual world to the screen. */
     def scalingStrategy: ScalingStrategy = _scalingStrategy
+
+    /** Set the scaling strategy for mapping the virtual world to the screen. */
     def scalingStrategy_= (strat: ScalingStrategy): Unit = {
       _scalingStrategy = strat
       update()
@@ -163,6 +191,31 @@ trait ViewportComponent {
       }
     }
   
+    /** Transform the canvas for drawing in the world coordinates.
+      *
+      * Applies the transformations needed to the canvas such that
+      * any rendering happening inside the scope can be done in the
+      * world coordinates and will end up being mapped properly
+      * to the screen with the viewport.
+      *
+      * A typical use of this is to initialize a Viewport to fill
+      * the entire Window (in pixels), then to set a camera of the size of the
+      * world we want to render (say with a unit of size 1). Finally in
+      * the render function, we prepare the canvas with the vithViewport
+      * method before doing the rendering in the world coordinate system.
+      * {{{
+      * val viewport = new Viewport(Window.Width, Window.height)
+      * viewport.setCamera(0, 0, 10, 10) // Our world is 10x10.
+      * viewport.scalingStrategy = Viewport.Fit
+      * ...
+      * override def render(canvas: Graphics.Canvas): Unit = {
+      *   viewport.withViewport(canvas) {
+      *     // Render our 10x10 world in its own coordinate system.
+      *     ...
+      *   }
+      * }
+      * }}}
+      */
     def withViewport(canvas: Graphics.Canvas)(body: => Unit): Unit = {
       // It would be nice to move this into Canvas and remove the dependency
       // to the GraphicsProvider, thus moving Viewport outside the Cake. But
@@ -170,7 +223,6 @@ trait ViewportComponent {
       // all internal state of the viewport, so it seems like it would be
       // the viewport responsability still.
       canvas.withSave{
-        canvas.translate(_cameraX, _cameraY)
         // TODO: screenX and screenY would allow to handle viewport as sub-part of the screen
         //       (like split-screen)
         canvas.clipRect(0, 0, screenWidth, screenHeight)
@@ -180,6 +232,7 @@ trait ViewportComponent {
 
         canvas.translate(offsetX, offsetY)
         canvas.scale(wRatio, hRatio)
+        canvas.translate(-_cameraX, -_cameraY)
 
         body
       }
@@ -216,30 +269,43 @@ trait ViewportComponent {
   }
 
   object Viewport {
+
+    /** The strategy for how to scale the world coordinates to the physical screen. */
     sealed trait ScalingStrategy
 
-    /** Scale the world to fit the viewport, does not maintain the aspect ratio.
-      * This will fully fill the viewport, but at the cost of breaking the aspect ratio. */
+    /** Scale the world to fit the viewport without maintaining the aspect ratio.
+      *
+      * This will fully fill the viewport, but at the cost of breaking the
+      * aspect ratio.  There won't be any blank borders.
+      */
     case object Stretch extends ScalingStrategy
 
     /** Scale the world to fit the viewport while keeping the aspect ratio.
-        This could leave some blank areas in order to maintain the aspect ratio.
-        At least one dimension of the world will fit the available space, while
-        on the other dimension the game will be centered with blank/black screen around */
+      *
+      * This could leave some blank areas in order to maintain the aspect
+      * ratio.  At least one dimension of the world will fit the available
+      * space, while on the other dimension the game will be centered with
+      * blank/black screen around.
+      */
     case object Fit extends ScalingStrategy
 
-    /** Scale the world to entirely fill the viewport while keeping the aspect ratio.
-        This could result in one dimension of the world to overflow the size of the viewport,
-        in that case the world will be centered along that dimension and will be cropped. */
-    case object Fill extends ScalingStrategy
-
-    /** Scale the world to fit the view port while keeping the aspect ratio.
-      * Similar to Fit, but instead of adding black bars this will keep the world
-      * in the top right and extend the world by the blank area.
+    /** Scale the world to fit the viewport while keeping the aspect ratio.
+      *
+      * Same scaling rules as [[Fit]], but instead of adding black bars this
+      * will keep the world in the top right and extend the world by the blank
+      * area.
       */
     case object Extend extends ScalingStrategy
 
-    /** No scaling applied to the world. */
+    /** Scale the world to entirely fill the viewport while keeping the aspect ratio.
+      *
+      * This could result in one dimension of the world overflowing the size of
+      * the viewport, in that case the world will be centered along that
+      * dimension and will be cropped.
+      */
+    case object Fill extends ScalingStrategy
+
+    /** Does not scale the world, maps world to screen 1:1. */
     case object NoScaling extends ScalingStrategy
   }
 
--- core/src/test/scala/sgl/ViewportSuite.scala
+++ core/src/test/scala/sgl/ViewportSuite.scala
@@ -18,7 +18,7 @@ class ViewportSuite extends AnyFunSuite {
     assert(viewport.scalingStrategy === Viewport.NoScaling)
   }
 
-  test("Viewport setCamera and translateCamera sets the right camera") {
+  test("Viewport setCamera, translateCamera, and translateCameraTo set the right camera") {
     val viewport = new Viewport(100, 150)
     viewport.setCamera(10, 20, 50, 70)
     assert(viewport.cameraX === 10)
@@ -33,6 +33,12 @@ class ViewportSuite extends AnyFunSuite {
     assert(viewport.cameraY === 45)
     assert(viewport.cameraWidth === 50)
     assert(viewport.cameraHeight === 70)
+
+    viewport.translateCameraTo(0, 0)
+    assert(viewport.cameraX === 0)
+    assert(viewport.cameraY === 0)
+    assert(viewport.cameraWidth === 50)
+    assert(viewport.cameraHeight === 70)
   }
 
   test("Viewport setScalingStrategy sets the right strategy") {
@@ -42,36 +48,153 @@ class ViewportSuite extends AnyFunSuite {
     assert(viewport.scalingStrategy === Viewport.Fit)
   }
 
-  test("Viewport correctly maps world coordinates to screen in Fit mode") {
+  test("Viewport correctly maps world coordinates to screen in Fit mode without any offset") {
+    val viewport = new Viewport(100, 200)
+
+    def testAllMappings(): Unit = {
+      viewport.setCamera(0, 0, 100, 200)
+      val (x1, y1) = viewport.worldToScreen(15, 25)
+      assert(x1 === 15)
+      assert(y1 === 25)
+
+      viewport.setCamera(0, 0, 50, 100)
+      val (x2, y2) = viewport.worldToScreen(20, 30)
+      assert(x2 === 40)
+      assert(y2 === 60)
+
+      viewport.setCamera(0, 0, 200, 400)
+      val (x3, y3) = viewport.worldToScreen(20, 30)
+      assert(x3 === 10)
+      assert(y3 === 15)
+
+      viewport.setCamera(50, 100, 100, 200)
+      val (x4, y4) = viewport.worldToScreen(65, 125)
+      assert(x4 === 15)
+      assert(y4 === 25)
+
+      viewport.setCamera(50, 100, 50, 100)
+      val (x5, y5) = viewport.worldToScreen(50, 100)
+      assert(x5 === 0)
+      assert(y5 === 0)
+      val (x6, y6) = viewport.worldToScreen(60, 130)
+      assert(x6 === 20)
+      assert(y6 === 60)
+    }
+
+    // None of the setCamera will require any black bar (offsets), so we
+    // should get the same results with any scaling strategy.
+    viewport.scalingStrategy = Viewport.Stretch
+    testAllMappings()
+    viewport.scalingStrategy = Viewport.Fit
+    testAllMappings()
+    viewport.scalingStrategy = Viewport.Fill
+    testAllMappings()
+    viewport.scalingStrategy = Viewport.Extend
+    testAllMappings()
+
+
+    // These were an attempt to set a reversed camera which
+    // would inverse some axis. This was working fine with
+    // coordinates mapping, but it wasn't useful in rendering
+    // due to all text/images being inverted in addition to just
+    // the axis. So the conclusions is that it was probably not the
+    // best approach to manage a world coordinate with different axis.
+    // Keeping the code around so that I don't forget about it (and
+    // because it never got checked in).
+    // set a camera setting up axis pointing top-left.
+    //viewport.setCamera(100, 200, 0, 0)
+    //val (x7, y7) = viewport.worldToScreen(0, 0)
+    //assert(x7 === 100)
+    //assert(y7 === 200)
+    //val (x8, y8) = viewport.worldToScreen(100, 200)
+    //assert(x8 === 0)
+    //assert(y8 === 0)
+    //val (x9, y9) = viewport.worldToScreen(25, 150)
+    //assert(x9 === 75)
+    //assert(y9 === 50)
+
+    //viewport.setCamera(200, 400, 0, 0)
+    //val (x10, y10) = viewport.worldToScreen(0, 0)
+    //assert(x10 === 100)
+    //assert(y10 === 200)
+    //val (x11, y11) = viewport.worldToScreen(200, 400)
+    //assert(x11 === 0)
+    //assert(y11 === 0)
+    //val (x12, y12) = viewport.worldToScreen(50, 300)
+    //assert(x12 === 75)
+    //assert(y12 === 50)
+
+    //viewport.setCamera(50, 100, 0, 0)
+    //val (x13, y13) = viewport.worldToScreen(0, 0)
+    //assert(x13 === 100)
+    //assert(y13 === 200)
+    //val (x14, y14) = viewport.worldToScreen(50, 100)
+    //assert(x14 === 0)
+    //assert(y14 === 0)
+    //val (x15, y15) = viewport.worldToScreen(10, 80)
+    //assert(x15 === 80)
+    //assert(y15 === 40)
+
+    //viewport.setCamera(200, 300, 100, 100)
+    //val (x16, y16) = viewport.worldToScreen(100, 100)
+    //assert(x16 === 100)
+    //assert(y16 === 200)
+    //val (x17, y17) = viewport.worldToScreen(200, 300)
+    //assert(x17 === 0)
+    //assert(y17 === 0)
+    //val (x18, y18) = viewport.worldToScreen(125, 250)
+    //assert(x18 === 75)
+    //assert(y18 === 50)
+
+    //// Let's try a camera with only one axis reversed.
+    //viewport.setCamera(0, 200, 100, 0)
+    //val (x19, y19) = viewport.worldToScreen(0, 0)
+    //assert(x19 === 0)
+    //assert(y19 === 200)
+    //val (x20, y20) = viewport.worldToScreen(100, 200)
+    //assert(x20 === 100)
+    //assert(y20 === 0)
+    //val (x21, y21) = viewport.worldToScreen(25, 150)
+    //assert(x21 === 25)
+    //assert(y21 === 50)
+
+    //viewport.setCamera(200, 0, 0, 400)
+    //val (x22, y22) = viewport.worldToScreen(0, 0)
+    //assert(x22 === 100)
+    //assert(y22 === 0)
+    //val (x23, y23) = viewport.worldToScreen(200, 400)
+    //assert(x23 === 0)
+    //assert(y23 === 200)
+    //val (x24, y24) = viewport.worldToScreen(50, 300)
+    //assert(x24 === 75)
+    //assert(y24 === 150)
+
+    //viewport.setCamera(50, 0, 0, 100)
+    //val (x25, y25) = viewport.worldToScreen(0, 0)
+    //assert(x25 === 100)
+    //assert(y25 === 0)
+    //val (x26, y26) = viewport.worldToScreen(50, 100)
+    //assert(x26 === 0)
+    //assert(y26 === 200)
+    //val (x27, y27) = viewport.worldToScreen(10, 75)
+    //assert(x27 === 80)
+    //assert(y27 === 150)
+  }
+
+  test("Viewport correctly maps world coordinates to screen in Fit mode with offset") {
     val viewport = new Viewport(100, 200)
     viewport.scalingStrategy = Viewport.Fit
 
-    val (x1, y1) = viewport.worldToScreen(15, 25)
-    assert(x1 === 15)
-    assert(y1 === 25)
-
-    viewport.setCamera(0, 0, 50, 100)
-    val (x2, y2) = viewport.worldToScreen(20, 30)
-    assert(x2 === 40)
-    assert(y2 === 60)
-
-    viewport.setCamera(0, 0, 200, 400)
-    val (x3, y3) = viewport.worldToScreen(20, 30)
-    assert(x3 === 10)
-    assert(y3 === 15)
-
-    viewport.setCamera(50, 100, 100, 200)
-    val (x4, y4) = viewport.worldToScreen(65, 125)
-    assert(x4 === 15)
-    assert(y4 === 25)
-
-    viewport.setCamera(50, 100, 50, 100)
-    val (x5, y5) = viewport.worldToScreen(50, 100)
-    assert(x5 === 0)
-    assert(y5 === 0)
-    val (x6, y6) = viewport.worldToScreen(60, 130)
-    assert(x6 === 20)
-    assert(y6 === 60)
+    viewport.setCamera(0, 0, 100, 100)
+    val (x1, y1) = viewport.worldToScreen(0, 0)
+    assert(x1 === 0)
+    assert(y1 === 50)
+    val (x2, y2) = viewport.worldToScreen(100, 100)
+    assert(x2 === 100)
+    assert(y2 === 150)
+    val (x3, y3) = viewport.worldToScreen(25, 75)
+    assert(x3 === 25)
+    assert(y3 === 125)
   }
 
   test("Viewport correctly maps screen coordinates to world in Fit mode") {
